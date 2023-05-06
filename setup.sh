921b3d8eaf585de25138d3b1a5ff6e7004221eed
anmalara <andrea.malara@cern.ch>
2023-05-06 12:53:48 +0200

    .gitignore

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
+source /cvmfs/cms.cern.ch/crab3/crab.sh
+voms-proxy-init -rfc -voms cms -valid 192:00
+
 source /cvmfs/sft.cern.ch/lcg/views/LCG_102/x86_64-centos7-gcc11-opt/setup.sh
 source bamboovenv/bin/activate
+
+export JMEVALIDATIONPATH=$(pwd -L)
+export PYTHONPATH=${PYTHONPATH}:${JMEVALIDATIONPATH}
