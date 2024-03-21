95c4ec012c09381b6075ff7510c1644e1aa7a3e4
Clemens Lange <clemens.lange@cern.ch>
2024-03-21 08:54:17 +0100

    Update repository URLs

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ source  /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc11-opt/setup.sh
 python -m venv bamboovenv
 source bamboovenv/bin/activate
 # clone and install bamboo
-git clone -o upstream https://gitlab.cern.ch/cp3-cms/bamboo.git
+git clone -o upstream https://gitlab.cern.ch/cms-analysis/general/bamboo.git
 pip install ./bamboo
 # clone and install correctionlib, CMSJMECalculators, plotIt
 pip install --no-binary=correctionlib correctionlib
-pip install git+ssh://git@gitlab.cern.ch:7999/cp3-cms/CMSJMECalculators.git
+pip install git+https://gitlab.cern.ch/cms-analysis/general/CMSJMECalculators.git
 git clone -o upstream https://github.com/cp3-llbb/plotIt.git
 mkdir build-plotit
 cd build-plotit
