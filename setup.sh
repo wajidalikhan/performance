b1075e02e45ccddb70cdd65de924c882906255bc
Anna Benecke <anna.benecke@cern.ch>
2024-08-08 19:06:25 +0200

    update to LCG_105 for non buggy root version

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 source /cvmfs/cms.cern.ch/crab3/crab.sh
 voms-proxy-init -rfc -voms cms -valid 192:00
 
-source /cvmfs/sft.cern.ch/lcg/views/LCG_104/x86_64-el9-gcc11-opt/setup.sh
+source /cvmfs/sft.cern.ch/lcg/views/LCG_105/x86_64-el9-gcc11-opt/setup.sh
 source bamboovenv/bin/activate
 
 export JMEVALIDATIONPATH=$(pwd -L)
