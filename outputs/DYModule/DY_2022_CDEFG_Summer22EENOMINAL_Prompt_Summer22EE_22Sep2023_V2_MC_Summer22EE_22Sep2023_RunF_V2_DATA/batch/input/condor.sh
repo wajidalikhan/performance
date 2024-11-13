3c230071205376e8aff34f510d0d0c269e706903
Wajid Ali Khan <wajid@cern.ch>
2024-11-13 06:16:24 +0100

    cleaning

--- outputs/DYModule/DY_2022_CDEFG_Summer22EENOMINAL_Prompt_Summer22EE_22Sep2023_V2_MC_Summer22EE_22Sep2023_RunF_V2_DATA/batch/input/condor.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-. /afs/cern.ch/work/w/wajid/METStudies/JMEFrameWrk/jme-validation/outputs/DYModule/DY_2022_CDEFG_Summer22EENOMINAL_Prompt_Summer22EE_22Sep2023_V2_MC_Summer22EE_22Sep2023_RunF_V2_DATA/batch/input/condor_$1.sh
